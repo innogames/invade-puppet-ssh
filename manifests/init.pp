9f1449c313e19dcbc5deea3e341975baf7c46250
Lennart Stein <lennart.stein@innogames.com>
2014-06-25 12:28:23 +0200

    Add ssh module script for puppet

---
+++ manifests/init.pp
@@ -0,0 +1,32 @@
+class ssh {
+  if $ssh_disabled == 'false' {
+    file {'copy-ssh-folder':
+      ensure        => 'directory',
+      recurse       => true,
+      path          => '/home/vagrant/.ssh',
+      owner         => 'vagrant',
+      group         => 'vagrant',
+      mode          => 0600,
+      sourceselect  => 'all',
+      source        => '/tmp/.ssh',
+      require       => Class['project::packages'],
+    }
+
+    file { '/tmp/known_hosts.sh':
+      ensure => file,
+      source => '/vagrant/puppet/modules/ssh/files/known_hosts.sh',
+      mode => 755,
+    }
+
+    exec { 'add-known-hosts':
+      command => '/tmp/known_hosts.sh',
+      provider => 'shell',
+      user => 'root',
+      require => [
+        File['/tmp/known_hosts.sh'],
+        File['copy-ssh-folder']
+      ],
+    }
+  }
+}
+
